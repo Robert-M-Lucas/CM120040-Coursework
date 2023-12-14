dc88546b7bdcfffc83e4825011905a89998558e0
Robert-App-Dev <robert01.appdev@gmail.com>
2023-12-14 12:46:27 +0000

    Push

--- main.py
+++ main.py
@@ -5,33 +5,34 @@ from flights import flight_options
 from others import others_options, OthersType
 from util import choices
 
-conn = sqlite3.connect("table.db")
-db_initialisation.initialise_db(conn)
+if __name__ == '__main__':
+    conn = sqlite3.connect("table.db")
+    db_initialisation.initialise_db(conn)
 
-while True:
-    c = choices("Select data:", [
-        "Flights",
-        "Pilots",
-        "Aircraft",
-        "Destinations",
-        "Save and Quit",
-        "Quit without Saving"
-    ])
+    while True:
+        c = choices("Select data:", [
+            "Flights",
+            "Pilots",
+            "Aircraft",
+            "Destinations",
+            "Save and Quit",
+            "Quit without Saving"
+        ])
 
-    if c == 1:
-        flight_options(conn)
-    elif c == 2:
-        others_options(conn, OthersType.PILOTS)
-    elif c == 3:
-        others_options(conn, OthersType.AIRCRAFT)
-    elif c == 4:
-        others_options(conn, OthersType.DESTINATIONS)
-    elif c == 5:
-        conn.commit()
-        break
-    elif c == 6:
-        c2 = choices("Are you sure you want to quit without saving?", ["Yes", "No"])
-        if c2 == 1:
+        if c == 1:
+            flight_options(conn)
+        elif c == 2:
+            others_options(conn, OthersType.PILOTS)
+        elif c == 3:
+            others_options(conn, OthersType.AIRCRAFT)
+        elif c == 4:
+            others_options(conn, OthersType.DESTINATIONS)
+        elif c == 5:
+            conn.commit()
             break
+        elif c == 6:
+            c2 = choices("Are you sure you want to quit without saving?", ["Yes", "No"])
+            if c2 == 1:
+                break
 
-conn.close()
+    conn.close()
